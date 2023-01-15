d61fda91331ac1961a43d1edc45564062fa92eb8
BleedingChips <chips_code@outlook.com>
2023-01-15 21:41:45 +0800

    Squashed commit of the following:
    commit ed1acc568f22b5fd54028017984a10c27eb10d94
    Author: BleedingChips <chips_code@outlook.com>
    Date:   Sun Jan 8 22:35:46 2023 +0800
    
        update
    
    commit 7a99e24c9f1957770455779b94b95f70648256fd
    Author: BleedingChips <chips_code@outlook.com>
    Date:   Tue Dec 13 23:19:45 2022 +0800
    
        update
    
    commit 48ef35b2e2a2b65e6a0f2d9abef03212222b63f3
    Author: BleedingChips <chips_code@outlook.com>
    Date:   Sat Nov 26 23:24:57 2022 +0800
    
        update CMake
    
    commit 9422540f95a698a7a4f387e5c6484ef2fd9aa193
    Author: BleedingChips <chips_code@outlook.com>
    Date:   Wed Nov 23 23:49:11 2022 +0800
    
        update CMake Framework
    
    commit 00e26789656be3eed28ec098e69ee1daea1e06bf
    Author: BleedingChips <chips_code@outlook.com>
    Date:   Tue Nov 22 23:36:20 2022 +0800
    
        update CMake

---
+++ include/UEBabyPram/UEBabyPramLogFilter.h
@@ -0,0 +1,89 @@
+#pragma once
+#include "Potato/PotatoStrFormat.h"
+#include "Potato/PotatoDocument.h"
+#include <string_view>
+
+namespace UEBabyPram::LogFilter
+{
+
+	template<typename Type = std::size_t>
+	using IndexSpan = Potato::Misc::IndexSpan<Type>;
+
+	struct LogTime
+	{
+		std::size_t Year = 0;
+		std::size_t Month = 0;
+		std::size_t Day = 0;
+		std::size_t Hour = 0;
+		std::size_t Min = 0;
+		std::size_t Sec = 0;
+		std::size_t MSec = 0;
+		std::size_t FrameCount = 0;
+	};
+
+	struct LogLine
+	{
+		std::u8string_view Time;
+		std::u8string_view Cagetory;
+		std::u8string_view Str;
+		std::u8string_view TotalStr;
+		IndexSpan<> LineIndex;
+	};
+
+	struct LogLineProcessor
+	{
+		struct Result
+		{
+			std::optional<LogLine> CaptureLine;
+			bool IsEnd;
+		};
+
+		std::optional<LogLine> Consume();
+
+		LogLineProcessor(std::u8string_view TotalStr);
+
+		operator bool() const {return Sperater || LastIndex.has_value(); }
+
+		static std::optional<LogTime> GetTime(LogLine InputLine);
+
+		void Clear();
+
+	private:
+
+		struct LogLineIndex
+		{
+			IndexSpan<> Time;
+			IndexSpan<> Category;
+			IndexSpan<> Str;
+			IndexSpan<> LineIndex;
+		};
+
+		static LogLineIndex Translate(Potato::Reg::HeadMatchProcessor::Result const& Re, std::size_t Offset, std::size_t StrSize, std::size_t LineOffset);
+
+		std::optional<LogLineIndex> LastIndex;
+		Potato::Reg::HeadMatchProcessor Pro;
+		Potato::Document::LineSperater Sperater;
+	};
+
+	template<typename Func>
+	std::size_t ForeachLogLine(LogLineProcessor& Pro, Func&& Fun) requires(std::is_invocable_v<Func&&, LogLine>)
+	{
+		Pro.Clear();
+		std::size_t Count = 0;
+		while (Pro)
+		{
+			auto Re = Pro.Consume();
+			assert(Re);
+			std::forward<Func>(Fun)(*Re);
+			++Count;
+		}
+		return Count;
+	}
+
+	template<typename Func>
+	std::size_t ForeachLogLine(std::u8string_view Str, Func&& Fun) requires(std::is_invocable_v<Func&&, LogLine>)
+	{
+		LogLineProcessor Pro(Str);
+		return ForeachLogLine(Pro, std::forward<Func>(Fun));
+	}
+}
---
+++ test/UnitTest.cpp
@@ -0,0 +1,23 @@
+#include "UEBabyPram/UEBabyPramLogFilter.h"
+#include <string_view>
+
+std::u8string_view Source = 
+u8R"(LogConfig: Setting CVar [[net.AllowAsyncLoading:1]]
+[2021.10.11-11.53.12:082][  0]LogConfig: Setting CVar [[con.DebugEarlyDefault:1]]
+)";
+
+
+int main()
+{
+
+	UEBabyPram::LogFilter::ForeachLogLine(Source, [](UEBabyPram::LogFilter::LogLine Line){
+		if (!Line.Time.empty())
+		{
+			auto Time = UEBabyPram::LogFilter::LogLineProcessor::GetTime(Line);
+			volatile int i = 0;
+		}
+		volatile int i = 0;
+	});
+
+	return 0;
+}
