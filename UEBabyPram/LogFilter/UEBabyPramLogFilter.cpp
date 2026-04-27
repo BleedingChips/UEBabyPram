4a5e38ba97d840078615a1c81702d1254f312b48
BleedingChips <chips_code@outlook.com>
2026-04-27 19:03:57 +0800

    Extend log filter EBNF and add test parse
    Expand the EBNF grammar to support string tokens, additional comparison operators and new statement types ($Level, $Line, $Log, $Category), as well as parentheses and boolean operators (&&, ||). Update TIME production and remove the old CONDITION token. Add a small test snippet in Test(): create an EbnfProcessor, set its observer table from the EBNF, and invoke Potato::EBNF::Process with a sample filter string; also add a placeholder mask vector. This prepares the log filter for richer expressions and a basic parse/processing test.

--- UEBabyPram/LogFilter/UEBabyPramLogFilter.cpp
+++ UEBabyPram/LogFilter/UEBabyPramLogFilter.cpp
@@ -5,19 +5,31 @@ import Potato;
 
 static constexpr std::u8string_view ebnf_string = u8R"(
 $:='\s+';
-CONDITION:='C[0-9]+' : [0];
 INT:='[1-9][0-9]+' : [1];
-COMPARE:='<' : [3];
-COMPARE:='<=' : [4];
-COMPARE:='==' : [5];
-COMPARE:='>' : [6];
-COMPARE:='>=':[7];
+STR:='\^.?[^\\]\^' : [2];
+STR:='@\^.?[^\\]\^' : [3];
+COMPARE:='<' : [4];
+COMPARE:='<=' : [5];
+COMPARE:='==' : [6];
+COMPARE:='>' : [7];
+COMPARE:='>=':[8];
 %%%%
-$:=<Exp>
-<TIME>:=INT '.' INT '.' INT ':' INT '.' INT '.' INT ':' INT : [1];
-<STAT>:='$Time' '<' <TIME> : [2];
+$:=<Exp>;
+<TIME>:=INT '.' INT '.' INT ':' INT : [2];
+<TIME>:=INT '.' INT '.' INT : [2];
+<STAT>:='$Time' COMPARE <TIME> : [2];
+<STAT>:='$Level' COMPARE <TIME> : [2];
+<STAT>:='$Line' COMPARE INT : [2];
+<STAT>:='$Log' '==' STR : [2];
+<STAT>:='$Category' '==' STR : [2];
+<STAT>:= '(' <STAT> ')' : [4];
+<STAT>:= <STAT> '&&' <STAT>  : [4];
+<STAT>:= <STAT> '||' <STAT>  : [4];
 <Exp>:=<STAT> ';' :[4];
 <Exp>:=;
+%%%%
++('&&');
++('||');
 )";
 
 namespace UEBabyPram::LogFilter
@@ -25,6 +37,10 @@ namespace UEBabyPram::LogFilter
 	void Test()
 	{
 		Potato::EBNF::Ebnf ebnf(ebnf_string);
+		std::pmr::vector<std::size_t> mask;
+		Potato::EBNF::EbnfProcessor pro;
+		pro.SetObserverTable(ebnf);
+		Potato::EBNF::Process(pro, std::u8string_view{ u8"$Log==^S1asas^ && $Line >= 1;" });
 		std::int32_t i = 0;
 	}
 }
