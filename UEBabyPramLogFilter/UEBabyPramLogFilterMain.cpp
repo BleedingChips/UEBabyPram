926c5df3e38801505e9ecc3f95f69b2168d62f7b
0w0 <chips_code@outlook.com>
2026-05-12 13:01:03 +0800

    Add LogFilter import, formatter & arg parsing
    Import UEBabyPramLogFilter and introduce a log category (log_filter) with a Potato::Log::LogCategoryFormatter specialization to prefix non-Display logs with timestamp, category and level. Add using namespace Potato, instantiate a FilterSetting, replace the previous argv debug prints with a stubbed CLI parser for -f/--file, and emit a startup log entry. TODO: implement file handling, validation and error reporting in the argument parsing.

--- UEBabyPramLogFilter/UEBabyPramLogFilterMain.cpp
+++ UEBabyPramLogFilter/UEBabyPramLogFilterMain.cpp
@@ -1,14 +1,58 @@
 import UEBabyPram;
+import UEBabyPramLogFilter;
 import Potato;
 import std;
 
+constexpr auto log_filter = Potato::TMP::TypeString(u8"LogFilter");
+
+namespace Potato::Log
+{
+	template<Potato::Log::LogLevel level>
+	struct LogCategoryFormatter<log_filter, level>
+	{
+		template<typename OutputIterator, typename ...Parameters>
+		OutputIterator operator()(OutputIterator iterator, std::basic_format_string<wchar_t, std::type_identity_t<Parameters>...> const& pattern, Parameters&& ...parameters)
+		{
+			if constexpr (level != Potato::Log::LogLevel::Display)
+			{
+				FormatedSystemTime time;
+				iterator = std::format_to(
+					std::move(iterator),
+					L"[{}]{}<{}>",
+					time, log_filter, level
+				);
+			}
+			return std::format_to(
+				std::move(iterator),
+				pattern,
+				std::forward<Parameters>(parameters)...
+			);
+		}
+	};
+}
+
+
 using namespace UEBabyPram;
+using namespace Potato;
 
 int main(int argc, char* argv[])
 {
+	
+	UEBabyPram::LogFilter::FilterSetting setting;
+
 	for (std::size_t i = 0; i < argc; ++i)
 	{
-		std::cout << argv[i] << std::endl;
+		if (argv[i] == "-f" || argv[i] == "--file")
+		{
+			if (i + 1 < argc)
+			{
+
+			}
+			else {
+				
+			}
+		}
 	}
+	Log::Log<log_filter, Log::LogLevel::Log, u8"Start Runing LogFilter">();
 	return 0;
 }
